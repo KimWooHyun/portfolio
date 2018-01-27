7331326ac6de78f65bc5decaf653f256d2506c1f
kimwoohyun <dngus242@gmail.com>
2018-01-27 22:36:43 +0900

    Fix meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: '김우현의 웹 포트폴리오' }
     ],
     link: [
       // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
